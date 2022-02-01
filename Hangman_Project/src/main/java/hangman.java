a7da1f7d428277cb88f17179497b3a7825087f3f
Steven <stevccru@icloud.com>
2022-02-01 14:02:12 -0500

    added hangman frame

--- Hangman_Project/src/main/java/hangman.java
+++ Hangman_Project/src/main/java/hangman.java
@@ -6,17 +6,33 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class hangman {
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("C:\\Users\\stevc\\OneDrive\\Documents\\hangman.txt"));
+    public static void main(String[] args)  {
+        System.out.println("Welcome to HANGMAN!");
+        System.out.println("+--+");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println(" ===");
+        Scanner scanner = null;
+
+        //The below line utilizes a try/catch to handle the program accepting the text file.
+        try {
+            scanner = new Scanner(new File("C:\\Users\\stevc\\OneDrive\\Documents\\hangman.txt"));
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("The requested file was not found");//throwing the error to the user
+            System.exit(0);//assigning a generic error status code for your reference.
+        }
         Scanner keyboard = new Scanner(System.in);
 
         List<String> words = new ArrayList<>();
 
 
             while (scanner.hasNext()) {
-                System.out.println(scanner.nextLine());
+
                 words.add(scanner.nextLine());
             }
+             System.out.println(words);
             Random ran = new Random();
 
             String word = words.get(ran.nextInt(words.size()));
@@ -25,14 +41,18 @@ public class hangman {
 
             List<Character> guesses = new ArrayList<>();
 
+        getGuesses(keyboard, word, guesses);
+
+
+    }
+
+    private static void getGuesses(Scanner keyboard, String word, List<Character> guesses) {
         printWordState(word, guesses);
-        System.out.println("Please enter a letter.");
+        System.out.println("Guess a letter.");
         String letterInput = keyboard.nextLine();
-       guesses.add(letterInput.charAt(0));
+        guesses.add(letterInput.charAt(0));
 
-       printWordState( word, guesses);
-
-
+        printWordState(word, guesses);
     }
 
     private static void printWordState(String word, List<Character> guesses) {
@@ -40,7 +60,7 @@ public class hangman {
             if (guesses.contains(word.charAt(i))) {
                 System.out.print(word.charAt(i));
             } else {
-                System.out.println("-");
+                System.out.print("-");
             }
 
         }
